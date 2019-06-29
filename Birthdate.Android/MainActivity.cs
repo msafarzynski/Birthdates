298c341d9cf725c1e3a84eae7920c45b5866e573
Maciek Safarzyński <msafarzynski@wp.pl>
2019-06-29 14:59:09 +0200

    Fixed GiftIdeas ListView UI composition; Locked on Portrait rotation

--- Birthdate.Android/MainActivity.cs
+++ Birthdate.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Birthdate.Droid
 {
-    [Activity(Label = "Birthdate", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Birthdate", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait))]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
